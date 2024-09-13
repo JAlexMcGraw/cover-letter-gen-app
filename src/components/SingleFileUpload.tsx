c1525602ec07a12267ff3481c4a02961e055a52e
Alex McGraw <jamcgraw21@gmail.com>
2024-09-13 17:28:31 -0500

    hotfix

--- src/components/SingleFileUpload.tsx
+++ src/components/SingleFileUpload.tsx
@@ -43,6 +43,7 @@ const SingleFileUploader: React.FC<SingleFileUploaderProps> = ({ resumeFile, res
     formData.append('file', resumeFile);
   
     try {
+      const apiUrl = import.meta.env.REACT_APP_API_URL;
       console.log('API_URL:', apiUrl);
       console.log('Full URL:', `${apiUrl}/api/upload-pdf/`);
 
