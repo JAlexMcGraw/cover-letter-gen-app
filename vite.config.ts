1abfbabfa1a776b6ee4a3fe01b8c20e2e5f3ff4c
Alex McGraw <jamcgraw21@gmail.com>
2024-09-13 16:59:37 -0500

    changing vercel, packag, and vite configs to hopefully deploy

--- vite.config.ts
+++ vite.config.ts
@@ -44,6 +44,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   define: {
     'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://localhost:8000'),
   },
