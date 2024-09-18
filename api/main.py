951cc0ad6ed67658f763198f9a64c67d474c1fb9
Alex McGraw <jamcgraw21@gmail.com>
2024-09-17 22:42:45 -0500

    add back middleware

--- api/main.py
+++ api/main.py
@@ -27,23 +27,23 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Setup CORS
-# origins = [
-#     "https://cover-letter-gen-app.vercel.app",
-#     "https://cover-letter-gen-616w1v7vg-alex-mcgraws-projects.vercel.app",
-#     "http://localhost:3000",  # Add this if you need local development
-#     "http://localhost:5173"
-# ]
+origins = [
+    "https://cover-letter-gen-app.vercel.app",
+    "https://cover-letter-gen-616w1v7vg-alex-mcgraws-projects.vercel.app",
+    "http://localhost:3000",  # Add this if you need local development
+    "http://localhost:5173"
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     # allow_origins=origins,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-#     # expose_headers=["*"],
-#     # max_age=600,  # Cache preflight requests for 10 minutes
-# )
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    # expose_headers=["*"],
+    # max_age=600,  # Cache preflight requests for 10 minutes
+)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
